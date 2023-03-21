8e2d0e092cb6a3828ef366d5c4b359f085bbf0f0
Vitali Bedik <vitalya.bedik@gmail.com>
2023-03-21 20:26:28 +0300

    feat: 10 video is done

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import './App.css';
 import Header from "./Header";
 import Technologies from "./Technologies";
+import Footer from "./Footer";
 
 const App = () => {
-  return (
-      <div>
-        <Header />
-        <Technologies/>
-      </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <Technologies/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
