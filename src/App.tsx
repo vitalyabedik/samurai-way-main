ba2ea7b7c8f339a6ac3aba00edbee646377ce104
Vitali Bedik <vitalya.bedik@gmail.com>
2023-04-01 18:16:21 +0300

    feat: 11 video is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,47 @@
 import React from 'react';
 import './App.css';
-import Header from "./Header";
-import Technologies from "./Technologies";
-import Footer from "./Footer";
+
+import logo from './assets/images/logo.svg'
+import profileBackground from './assets/images/profile-background.png'
+
 
 const App = () => {
     return (
-        <div>
-            <Header/>
-            <Technologies/>
-            <Footer/>
+        <div className="app-wrapper">
+            <header className="header">
+                <img src={logo} alt="logo"/>
+            </header>
+            <div className="container">
+                <aside className="aside">
+                    <ul className="navigation__items">
+                        <li className="navigation__item">
+                            <a className="navigation__link" href="#">Profile</a>
+                        </li>
+                        <li className="navigation__item">
+                            <a className="navigation__link" href="#">Messages</a>
+                        </li>
+                        <li className="navigation__item">
+                            <a className="navigation__link" href="#">News</a>
+                        </li>
+                        <li className="navigation__item">
+                            <a className="navigation__link" href="#">Music</a>
+                        </li>
+                        <li className="navigation__item">
+                            <a className="navigation__link" href="#">Settings</a>
+                        </li>
+                    </ul>
+                </aside>
+                <main className="main">
+                    <img src={profileBackground} alt="profile-background"/>
+                    <div>avatar + descr</div>
+                    <div>My posts</div>
+                    <div>New post</div>
+                    <div>
+                        <div>Post1</div>
+                        <div>Post2</div>
+                    </div>
+                </main>
+            </div>
         </div>
     );
 }
