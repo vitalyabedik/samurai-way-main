a3d87d0853d3767516540643d6b86d64e4c0bbd3
Vitali Bedik <vitalya.bedik@gmail.com>
2023-04-09 18:43:25 +0300

    feat: add styles for Header and Aside components

--- src/components/Aside/Aside.tsx
+++ src/components/Aside/Aside.tsx
@@ -2,26 +2,59 @@ import {NavLink} from 'react-router-dom';
 
 import styles from './Aside.module.css';
 
+import iconProfile from '../../assets/images/aside/icon-profile.svg'
+import iconMessages from '../../assets/images/aside/icon-messages.svg'
+import iconNews from '../../assets/images/aside/icon-news.svg'
+import iconMusic from '../../assets/images/aside/icon-music.svg'
+import iconSettings from '../../assets/images/aside/icon-settings.svg'
+
 export const Aside = () => {
     return (
         <aside className={styles.root}>
-            <ul className={styles.items}>
-                <li className={styles.item}>
-                    <NavLink to="/profile" className={styles.link} activeClassName={styles.link_active}>Profile</NavLink>
-                </li>
-                <li className={styles.item}>
-                    <NavLink to="/messages" className={styles.link} activeClassName={styles.link_active}>Messages</NavLink>
-                </li>
-                <li className={styles.item}>
-                    <NavLink to="/news" className={styles.link} activeClassName={styles.link_active}>News</NavLink>
-                </li>
-                <li className={styles.item}>
-                    <NavLink to="/music" className={styles.link} activeClassName={styles.link_active}>Music</NavLink>
-                </li>
-                <li className={styles.item}>
-                    <NavLink to="/settings" className={styles.link} activeClassName={styles.link_active}>Settings</NavLink>
-                </li>
-            </ul>
+            <div className={styles.wrapper}>
+                <ul className={styles.items}>
+                    <li className={styles.item}>
+                        <NavLink to="/profile" className={styles.item__link} activeClassName={styles.item__link_active}>
+                            <div className={styles.item__image}>
+                                <img src={iconProfile} alt="icon-profile"/>
+                            </div>
+                            <span>Profile</span>
+                        </NavLink>
+                    </li>
+                    <li className={styles.item}>
+                        <NavLink to="/messages" className={styles.item__link} activeClassName={styles.item__link_active}>
+                            <div className={styles.item__image}>
+                                <img src={iconMessages} alt="icon-messages"/>
+                            </div>
+                            <span>Messages</span>
+                        </NavLink>
+                    </li>
+                    <li className={styles.item}>
+                        <NavLink to="/news" className={styles.item__link} activeClassName={styles.item__link_active}>
+                            <div className={styles.item__image}>
+                                <img src={iconNews} alt="icon-news"/>
+                            </div>
+                            <span>News</span>
+                        </NavLink>
+                    </li>
+                    <li className={styles.item}>
+                        <NavLink to="/music" className={styles.item__link} activeClassName={styles.item__link_active}>
+                            <div className={styles.item__image}>
+                                <img src={iconMusic} alt="icon-music"/>
+                            </div>
+                            <span>Music</span>
+                        </NavLink>
+                    </li>
+                    <li className={styles.item}>
+                        <NavLink to="/settings" className={styles.item__link} activeClassName={styles.item__link_active}>
+                            <div className={styles.item__image}>
+                                <img src={iconSettings} alt="icon-settings"/>
+                            </div>
+                            <span>Settings</span>
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
         </aside>
     )
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,27 @@
 import styles from './Header.module.css';
 
-import logo from '../../assets/images/logo.svg';
+import menuIcon from '../../assets/images/header/icon-menu.svg';
+import logo from '../../assets/images/header/logo.png';
+import iconSearch from '../../assets/images/header/icon-search.svg';
 
 
 export const Header = () => {
     return (
         <header className={styles.root}>
-            <img src={logo} alt="logo"/>
+                <div className={styles.logoBlock}>
+                    <div className={styles.menuIcon}>
+                        <img src={menuIcon} alt="menu"/>
+                    </div>
+                    <div className={styles.menuLogo}>
+                        <img src={logo} alt="logo"/>
+                    </div>
+                </div>
+                <div className={styles.searchBlock}>
+                    <div className={styles.searchIcon}>
+                        <img src={iconSearch} alt="icon-search"/>
+                    </div>
+                    <input className={styles.searchInput}/>
+                </div>
         </header>
     )
 }
