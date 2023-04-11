4e8c70f3bcf0e140a411c11d7317c54133ea4ba7
Vitali Bedik <vitalya.bedik@gmail.com>
2023-04-11 16:31:30 +0300

    feat: 24 video is done (create BLL for dialogs, messages, posts)

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,12 +2,44 @@ import {NavLink} from 'react-router-dom';
 
 import styles from './Dialogs.module.css'
 
-type DialogItemPropsType = {
+type DialogType = {
     id: number
     name: string
 }
 
-const DialogItem = (props: DialogItemPropsType) => {
+type MessageType = {
+    id: number
+    message: string
+}
+
+type DialogPropsType = {
+    dialogsData: Array<DialogType>
+    messagesData: Array<MessageType>
+}
+
+const dialogsData = [
+    {id: 1, name: 'Dennis Han'},
+    {id: 2, name: 'Erica Jones'},
+    {id: 3, name: 'Alex Dolgove'},
+    {id: 4, name: 'Alex Dolgove'},
+    {id: 5, name: 'Stella Johnson'},
+    {id: 6, name: 'Alex Dolgove'},
+    {id: 7, name: 'Stella Johnson'},
+    {id: 8, name: 'Dennis Han'},
+]
+
+const messagesData = [
+    {id: 1, message: 'Hi'},
+    {id: 2, message: 'Hello'},
+    {id: 3, message: 'How are you?'},
+    {id: 4, message: 'What happened?'},
+    {id: 5, message: 'How can i do it?'},
+    {id: 6, message: 'Yo'},
+    {id: 7, message: 'Hello'},
+    {id: 8, message: 'How are you?'},
+]
+
+const DialogItem = (props: DialogType) => {
     let path = `/dialogs/${props.id}`
 
     return (
@@ -17,11 +49,7 @@ const DialogItem = (props: DialogItemPropsType) => {
     )
 }
 
-type MessageItemPropsType = {
-    message: string
-}
-
-const MessageItem = (props: MessageItemPropsType) => {
+const MessageItem = (props: MessageType) => {
     return (
         <div className={styles.message__item}>
             {props.message}
@@ -29,27 +57,32 @@ const MessageItem = (props: MessageItemPropsType) => {
     )
 }
 
-export const Dialogs = (props: any) => {
+export const Dialogs = (props: DialogPropsType) => {
+
     return (
         <div className={styles.root}>
             <div className={styles.dialogs}>
                 <div className={styles.dialogs__items}>
-                    <DialogItem id={1} name={'Vitali'}/>
-                    <DialogItem id={2} name={'Natasha'}/>
-                    <DialogItem id={3} name={'Gena'}/>
-                    <DialogItem id={4} name={'Larisa'}/>
-                    <DialogItem id={5} name={'Valera'}/>
-                    <DialogItem id={6} name={'Vlad'}/>
+                    <DialogItem id={dialogsData[0].id} name={dialogsData[0].name}/>
+                    <DialogItem id={dialogsData[1].id} name={dialogsData[1].name}/>
+                    <DialogItem id={dialogsData[2].id} name={dialogsData[2].name}/>
+                    <DialogItem id={dialogsData[3].id} name={dialogsData[3].name}/>
+                    <DialogItem id={dialogsData[4].id} name={dialogsData[4].name}/>
+                    <DialogItem id={dialogsData[5].id} name={dialogsData[5].name}/>
+                    <DialogItem id={dialogsData[6].id} name={dialogsData[6].name}/>
+                    <DialogItem id={dialogsData[7].id} name={dialogsData[7].name}/>
                 </div>
             </div>
             <div className={styles.messages}>
                 <div className={styles.messages__items}>
-                    <MessageItem message="Hi"/>
-                    <MessageItem message="Hello"/>
-                    <MessageItem message="How are you?"/>
-                    <MessageItem message="What happened?"/>
-                    <MessageItem message="How can i do it?"/>
-                    <MessageItem message="Yo"/>
+                    <MessageItem id={messagesData[0].id} message={messagesData[0].message}/>
+                    <MessageItem id={messagesData[1].id} message={messagesData[1].message}/>
+                    <MessageItem id={messagesData[2].id} message={messagesData[2].message}/>
+                    <MessageItem id={messagesData[3].id} message={messagesData[3].message}/>
+                    <MessageItem id={messagesData[4].id} message={messagesData[4].message}/>
+                    <MessageItem id={messagesData[5].id} message={messagesData[5].message}/>
+                    <MessageItem id={messagesData[6].id} message={messagesData[6].message}/>
+                    <MessageItem id={messagesData[7].id} message={messagesData[7].message}/>
                 </div>
             </div>
         </div>
--- src/components/Profile/Posts/Posts.tsx
+++ src/components/Profile/Posts/Posts.tsx
@@ -4,6 +4,11 @@ import {Post} from './Post/Post';
 
 
 export const Posts = () => {
+    const postData = [
+        {id: 1, message:  "Hi, how are you?", likesCount: 4},
+        {id: 2, message:  'It\'s my first post?', likesCount: 10},
+    ]
+
     return (
         <div className={styles.timeline}>
             <div className={styles.createPost}>
@@ -11,8 +16,8 @@ export const Posts = () => {
                 <button>Add post</button>
             </div>
             <div className={styles.posts}>
-                <Post message="Hi, how are you?" likes={4}/>
-                <Post message={'It\'s my first post?'} likes={10}/>
+                <Post message={postData[0].message} likes={postData[0].likesCount}/>
+                <Post message={postData[1].message} likes={postData[1].likesCount}/>
             </div>
         </div>
     )
