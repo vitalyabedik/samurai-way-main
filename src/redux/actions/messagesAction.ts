58e6df833ddfad545b14db2905b61f1203604b5d
Vitali Bedik <vitalya.bedik@gmail.com>
2023-05-01 18:21:16 +0300

    fix: refactor actionCreators

--- src/redux/actions/messagesAction.ts
+++ src/redux/actions/messagesAction.ts
@@ -1,17 +1,12 @@
 import {MESSAGES_ADD, MESSAGES_UPDATE_NEW_TEXT} from './actionTypes';
 
-
 export type MessagesActionType = ReturnType<typeof addMessageAC> | ReturnType<typeof changeNewMessageTextAC>
 
-export const addMessageAC = () => {
-    return {
-        type: MESSAGES_ADD
-    } as const
-}
+export const addMessageAC = () => ({
+    type: MESSAGES_ADD
+} as const)
 
-export const changeNewMessageTextAC = (newMessageText: string) => {
-    return {
-        type: MESSAGES_UPDATE_NEW_TEXT,
-        newMessageText
-    } as const
-}
+export const changeNewMessageTextAC = (newMessageText: string) => ({
+    type: MESSAGES_UPDATE_NEW_TEXT,
+    newMessageText
+} as const)
--- src/redux/actions/postAction.ts
+++ src/redux/actions/postAction.ts
@@ -2,15 +2,11 @@ import {POST_ADD, POST_UPDATE_NEW_TEXT} from './actionTypes';
 
 export type PostActionType = ReturnType<typeof addPostAC> | ReturnType<typeof changeNewPostTextAC>
 
-export const addPostAC = () => {
-    return {
-        type: POST_ADD
-    } as const
-}
+export const addPostAC = () => ({
+    type: POST_ADD
+} as const)
 
-export const changeNewPostTextAC = (newText: string) => {
-    return {
-        type: POST_UPDATE_NEW_TEXT,
-        newText
-    } as const
-}
+export const changeNewPostTextAC = (newText: string) => ({
+    type: POST_UPDATE_NEW_TEXT,
+    newText
+} as const);
