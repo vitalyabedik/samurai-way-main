b547d250e86e0e41c68106af093e050331a8ae86
Vitali Bedik <vitalya.bedik@gmail.com>
2023-09-16 12:23:45 +0300

    feat: add styles for Preloader

--- src/components/common/Preloader/Preloader.tsx
+++ src/components/common/Preloader/Preloader.tsx
@@ -2,13 +2,9 @@ import React from 'react';
 
 import styles from './Preloader.module.css'
 
-import preloader from '../../../assets/images/preloader.gif'
-
-export const Preloader = () => {
+export const Preloader: React.FC = () => {
     return (
-        <div className={styles.root}>
-            <img src={preloader}/>
-        </div>
+        <span className={styles.loader}></span>
     );
 };
 
